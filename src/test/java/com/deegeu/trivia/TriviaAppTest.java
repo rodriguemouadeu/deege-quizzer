04bd2a2d055c227bcca233d9a7e8b38d3af2c873
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2018-05-07 14:16:25 +0200

    Initial commit

---
+++ src/test/java/com/deegeu/trivia/TriviaAppTest.java
@@ -0,0 +1,20 @@
+package com.deegeu.trivia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple TriviaApp.
+ */
+public class TriviaAppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
